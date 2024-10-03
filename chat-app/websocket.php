b2be6567b3f344385a0969c74fe199d2bd48beab
crashbandicootsthe1 <144353514+crashbandicootsthe1@users.noreply.github.com>
2024-10-02 21:26:30 -1100

    Create websocket.php

---
+++ chat-app/websocket.php
@@ -0,0 +1,36 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+require 'vendor/autoload.php';
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(new HttpServer(new WsServer(new Chat())), 8080);
+$server->run();
